164fec2f518a8e6cec946d3c279e66bb0abc9ff1
nikhilhore <horenikhil5@gmail.com>
2021-09-15 23:27:42 +0530

    bug fix trial 2

--- site/main.js
+++ site/main.js
@@ -1,19 +1,19 @@
+var sectionids = ["sectionh", "sectiona", "sectionw", "sectionr", "sectionc"];
+
 function dropdowntoggle() {
     document.getElementById("mydropdown").classList.toggle("show");
 }
 
 function addHideClass(sectionid) {
-    let sectionids = ["sectionh", "sectiona", "sectionw", "sectionr", "sectionc"];
     let n = sectionids.length;
     for (let i = 0; i < n; i++) {
         let sec = document.getElementById(sectionids[i]);
-        sec.classList.add("hidden");
+        if (!sec.classList.contains('hidden')){
+            sec.classList.add("hidden");
+        }
     }
     let sect = document.getElementById(sectionid);
     sect.classList.remove("hidden");
-    sect= document.getElementById('sectionh');
-    sect.classList.remove("show");
-    sect.classList.add("hidden");
 }
 
 window.onclick = function (event) {
