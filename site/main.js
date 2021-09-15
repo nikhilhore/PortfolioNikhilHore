08f717c7f3825966221b9c6c183c557e6f9f90bb
nikhilhore <horenikhil5@gmail.com>
2021-09-15 23:36:58 +0530

    bug fixed

--- site/main.js
+++ site/main.js
@@ -1,19 +1,15 @@
-var sectionids = ["sectionh", "sectiona", "sectionw", "sectionr", "sectionc"];
-
 function dropdowntoggle() {
     document.getElementById("mydropdown").classList.toggle("show");
 }
 
+var sectionids = ["sectionh", "sectiona", "sectionw", "sectionr", "sectionc"];
+
 function addHideClass(sectionid) {
     let n = sectionids.length;
     for (let i = 0; i < n; i++) {
-        let sec = document.getElementById(sectionids[i]);
-        if (!sec.classList.contains('hidden')){
-            sec.classList.add("hidden");
-        }
+        document.getElementById(sectionids[i]).classList.add("hidden");
     }
-    let sect = document.getElementById(sectionid);
-    sect.classList.remove("hidden");
+    document.getElementById(sectionid).classList.remove("hidden");
 }
 
 window.onclick = function (event) {
