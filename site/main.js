94ee89c7f70ff782dde84825ec3c97ff0c1514ca
nikhilhore <horenikhil5@gmail.com>
2021-09-15 20:03:24 +0530

    New stylesheet

--- site/main.js
+++ site/main.js
@@ -2,16 +2,26 @@ function dropdowntoggle() {
     document.getElementById("mydropdown").classList.toggle("show");
 }
 
-function addHideClass(sectionid){
-    let sectionids= ["sectionh", "sectiona", "sectionw", "sectionr", "sectionc"];
-    let n= sectionids.length;
-    // let n= 5;
-    for (let i=0; i<n; i++){
-        let sec= document.getElementById(sectionids[i]);
-        sec.classList.add("hide");
+function addHideClass(sectionid) {
+    let sectionids = ["sectionh", "sectiona", "sectionw", "sectionr", "sectionc"];
+    let n = sectionids.length;
+    for (let i = 0; i < n; i++) {
+        let sec = document.getElementById(sectionids[i]);
+        sec.classList.add("hidden");
     }
-    let sect= document.getElementById(sectionid);
-    sect.classList.remove("hide");
+    let sect = document.getElementById(sectionid);
+    sect.classList.remove("hidden");
 }
 
-// section h,a,w,r,c
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
