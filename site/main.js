ebae98acc098296d658e271ffb0bd3d748d27157
nikhilhore <horenikhil5@gmail.com>
2021-09-15 14:25:14 +0530

    complete minus work version 1

--- site/main.js
+++ site/main.js
@@ -1,3 +1,17 @@
 function dropdowntoggle() {
     document.getElementById("mydropdown").classList.toggle("show");
 }
+
+function addHideClass(sectionid){
+    let sectionids= ["sectionh", "sectiona", "sectionw", "sectionr", "sectionc"];
+    let n= sectionids.length;
+    // let n= 5;
+    for (let i=0; i<n; i++){
+        let sec= document.getElementById(sectionids[i]);
+        sec.classList.add("hide");
+    }
+    let sect= document.getElementById(sectionid);
+    sect.classList.remove("hide");
+}
+
+// section h,a,w,r,c
