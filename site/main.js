f42979dbeefd6d9ed020cad4447482c0bbaa62e7
nikhilhore <horenikhil5@gmail.com>
2021-09-15 23:06:43 +0530

    Bug fixes

--- site/main.js
+++ site/main.js
@@ -18,10 +18,7 @@ window.onclick = function (event) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
         for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
+            dropdowns[i].classList.remove('show');
         }
     }
 }
